f1ea0093b25b28650f428676f50ab70b331135e4
Sourabh <sourabhwaikar1992@gmail.com>
2024-12-30 16:14:51 +0530

    Shell script update to remove get_helm.sh directory after installation

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -77,6 +77,7 @@ if ! command -v helm &> /dev/null; then
     curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
     chmod 700 get_helm.sh
     ./get_helm.sh
+    rm -rf get_helm.sh # Clean up the the directory
 else
     echo "Helm version: $(helm version)"
 fi
