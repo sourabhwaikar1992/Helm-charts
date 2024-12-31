a5d5c1c54d4b5187166fc08e7321d21847405e45
Sourabh <sourabhwaikar1992@gmail.com>
2024-12-31 15:55:52 +0530

    EKS cluster creation script

---
+++ scripts/EKS/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define variables
+CLUSTER_NAME="my-cluster"
+NODE_TYPE="t3.medium"
+NODES="3"
+MIN_NODES="1"
+REGION="ap-south-1"
+INGRESS_NAME="my-ingress"  # Default ingress name is 'my-ingress'
+K8S_VERSION=1.31.0
